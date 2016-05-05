0879c4d318c45e585130999c712aac0a3de6114f
Peter Rupa <megatricycle@gmail.com>
2016-05-05 23:21:54 +0800

    Refactor printing code

--- icsvim.c
+++ icsvim.c
@@ -5,59 +5,57 @@
 #define BODY_HEIGHT 20
 #define TILDE_WIDTH 2
 
-void render() {
-    int i, j;
-    int current_line_number = 1; // hardcoded
-    int max_line_number = 1; // hardcoded as of now
+void print_top_bar() {
+    int i;
     
-    clrscr();
-    
-    // render top bar
     for(i = 0; i < TERMINAL_WIDTH; i++) {
         textbackground(LIGHTGRAY);
         printf(" ");
     }
-    
-    // render valid lines
-    for(i = 0; i < max_line_number; i++) {
-        // @TODO: Make width dynamic
+}
+
+void print_line(int line_number) {
+    // @TODO: Make width dynamic
+    int i;
         
+    textbackground(BLACK);
+    // render line number
+    for(i = 0; i < 3; i++) {
         textbackground(BLACK);
-        // render line number
-        for(j = 0; j < 3; j++) {
-            textbackground(BLACK);
-            printf(" ");
-        }
-        
-        textcolor(YELLOW);
-        printf("%i", i + 1);
-        
-        for(j = 0; j < TERMINAL_WIDTH - 4; j++) {
-            textbackground(BLACK);
-            textcolor(WHITE);
-            printf(" ");
-        }
+        printf(" ");
     }
     
-    // render empty lines
-    for(i = 0; i < BODY_HEIGHT - max_line_number; i++) {
+    textcolor(YELLOW);
+    printf("%i", line_number);
+    
+    for(i = 0; i < TERMINAL_WIDTH - 4; i++) {
         textbackground(BLACK);
-        textcolor(BLUE);
-        printf(" ~");
-        
-        for(j = 0; j < TERMINAL_WIDTH - TILDE_WIDTH; j++) {
-            textbackground(BLACK);
-            textcolor(WHITE);
-            printf(" ");
-        }
+        textcolor(WHITE);
+        printf(" ");
     }
+}
+
+void print_empty_line() {
+    int i;
     
-    // render lower bar
-    textbackground(WHITE);
+    textbackground(BLACK);
+    textcolor(BLUE);
+    printf(" ~");
     
-    char filename[] = "dummyfile.txt";
+    for(i = 0; i < TERMINAL_WIDTH - TILDE_WIDTH; i++) {
+        textbackground(BLACK);
+        textcolor(WHITE);
+        printf(" ");
+    }
+}
+
+void print_status_bar(char *filename, int current_line_number) {
+    int i;
+    
     char status_message[80];
     char current_line_number_buffer[30];
+    
+    textbackground(WHITE);
     
     // print status message
     textcolor(BLACK);
@@ -66,13 +64,39 @@ void render() {
     strcat(status_message, "]  Line: ");
     itoa(current_line_number, current_line_number_buffer);
     strcat(status_message, current_line_number_buffer);
-    printf("%s", status_message); 
+    printf("%s", status_message);
     
     for(i = 0; i < TERMINAL_WIDTH - strlen(status_message); i++) {
         printf(" ");
     }
     textbackground(BLACK);
     printf(" ");
+}
+
+void render() {
+    int i, j;
+    int current_line_number = 1; // hardcoded
+    int max_line_number = 1; // hardcoded as of now
+    
+    clrscr();
+    
+    // render top bar
+    print_top_bar();
+    
+    // render valid lines
+    for(i = 0; i < max_line_number; i++) {
+        print_line(i + 1);
+    }
+    
+    // render empty lines
+    for(i = 0; i < BODY_HEIGHT - max_line_number; i++) {
+        print_empty_line();
+    }
+    
+    char filename[] = "dummyfile.txt"; // hardcoded
+    
+    // render lower bar
+    print_status_bar(filename, current_line_number);
     
     // render command line
     textcolor(WHITE);
